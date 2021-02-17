3f836f9fc21577e28edd5c524a34065d2ffefd7c
umbarumba <unlimited.pike@gmail.com>
2021-02-17 20:36:40 +0900

    ダミー

--- team_hands-on/src/index.js
+++ team_hands-on/src/index.js
@@ -128,7 +128,9 @@
 	/* 演習の編集範囲 はじめ */
 
 	/* 演習1 ここから */
-	function mul(){eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
+
+	function dummy(){
+		//消してね
 	}
 	/* 演習1 ここまで */
 
