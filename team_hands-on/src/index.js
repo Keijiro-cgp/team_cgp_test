efd180bea09ac565e5693154c0b7de1b4e8fdace
Ririri15_niko <77618140+ririri15@users.noreply.github.com>
2021-02-17 20:30:56 +0900

    Update index.js

--- team_hands-on/src/index.js
+++ team_hands-on/src/index.js
@@ -128,8 +128,7 @@
 	/* 演習の編集範囲 はじめ */
 
 	/* 演習1 ここから */
-	function sub(){
-		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
+	function mul(){eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
 	}
 	/* 演習1 ここまで */
 
